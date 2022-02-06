c5d93016f340b26638d6e7a837a1a652f4bac8cc
Philip <philipkristoffersen@gmail.com>
2022-02-06 15:00:48 +0100

    suggest guesses based on score

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::{HashMap, HashSet},
     io::{self, BufRead},
 };
 
@@ -7,8 +8,15 @@ fn main() {
     let stdin = io::stdin();
     let mut current_word: Option<String> = None;
     let mut current_colors: Option<String> = None;
+    sort_list_by_score(&mut words);
 
     println!("Write your guess (5 letter limit, then hit enter)");
+    println!("Try one of theese");
+    for word in words.iter().take(5) {
+        println!("{}", word);
+    }
+    println!("");
+
     for line in stdin.lock().lines() {
         let line = line.unwrap();
 
@@ -26,21 +34,33 @@ fn main() {
             let requirements = string_to_requirements(input, word).unwrap();
             words.retain(|word| word_matches_requirements(word, &requirements));
 
+            sort_list_by_score(&mut words);
             current_colors = None;
             current_word = None;
             println!("{} possible words remaining", words.len());
-
-            if words.len() < 10 {
-                println!("Try one of theese");
-                for word in &words {
-                    println!("{}", word);
-                }
+            println!("Try one of theese");
+            for word in words.iter().take(5) {
+                println!("{}", word);
             }
             println!("");
 
             println!("Write your guess (5 letter limit, then hit enter)");
         }
     }
+}
+
+fn sort_list_by_score(words: &mut Vec<String>) {
+    let all_words = words.clone();
+    let mut score_map = HashMap::new();
+    for current_word in &all_words {
+        let mut word_score = 0;
+        for target in &all_words {
+            word_score += calculate_score(current_word, target);
+        }
+        score_map.insert(current_word, word_score);
+    }
+    words.sort_by_cached_key(|k| score_map.get(k));
+    words.reverse();
 }
 
 fn string_to_requirements<N: AsRef<str>>(input: N, word: N) -> Result<[Requirement; 5], String> {
@@ -106,16 +126,70 @@ fn word_matches_requirement<N: AsRef<str>>(word: N, index: usize, req: &Requirem
 fn create_word_list() -> Vec<String> {
     let words_string = include_str!("../sgb-words.txt");
     let lines = words_string.split("\n");
-    lines
-        .into_iter()
-        .map(|s| s.to_string())
-        .collect()
+    lines.into_iter().map(|s| s.to_string()).collect()
 }
 
+fn calculate_score<N: AsRef<str>, M: AsRef<str>>(word: N, target: M) -> u32 {
+    let word = word.as_ref();
+    let target = target.as_ref();
+    let has_doubles = has_doubles(word);
+    let mut sum = 0;
+    for (index, char) in word.chars().into_iter().enumerate() {
+        let req = char_requirement(index, char.to_string(), target);
+        sum += match req {
+            Requirement::Green(_) => 3,
+            Requirement::Yellow(_) => {
+                if has_doubles {
+                    0
+                } else {
+                    2
+                }
+            }
+            Requirement::Black(_) => 0,
+        }
+    }
+    return sum;
+}
+
+fn char_requirement<N: AsRef<str>, M: AsRef<str>>(index: usize, char: N, target: M) -> Requirement {
+    let target = target.as_ref();
+    let char = char.as_ref();
+    if !target.contains(char) {
+        return Requirement::Black(char.to_string());
+    } else {
+        if target[index..index + 1].eq(char) {
+            return Requirement::Green(char.to_string());
+        } else {
+            return Requirement::Yellow(char.to_string());
+        }
+    }
+}
+
+fn has_doubles<N: AsRef<str>>(word: N) -> bool {
+    let mut set = HashSet::new();
+    let word = word.as_ref();
+    for char in word.chars() {
+        if set.contains(&char) {
+            return true;
+        } else {
+            set.insert(char);
+        }
+    }
+    return false;
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn can_calculate_score() {
+        let score = calculate_score("cares", "cotte");
+        assert_eq!(4, score);
+
+        let score = calculate_score("pluto", "cares");
+        assert_eq!(0, score);
+    }
 
     #[test]
     fn has_no_blacks() {
